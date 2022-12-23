e6f8cf68e1637f7353f1859edb1d6052f3c711ff
Kyrian <dr.kyrian.onwe@gmail.com>
2022-12-23 14:08:51 -0600

    Cleanup ReviewsPage

--- client/src/components/ReviewsPage.jsx
+++ client/src/components/ReviewsPage.jsx
@@ -5,19 +5,10 @@ import { useNavigate } from 'react-router-dom'
 
 function ReviewsPage({ manga, reset, currUse, mangas, setVo, add }) {
   const [obj, setObj] = useState(manga)
-  // all the reviews of a single manga should be visible here and there should be an 'add a review' button, in addition to 'like' or 'dislike' button
-  // let obj = manga.x[0][0]
+
   console.log(manga)
   const hist = useNavigate()
 
-  // function resetAddReview(data){
-  //   let reviewhold = obj.review 
-  //   let newRev = [...reviewhold, data]
-  //   let newObj = obj
-  //   newObj.reviews = newRev
-  //   setObj(newObj)
-
-  // }
 
   console.log(obj)
   let revis = obj.reviews
@@ -42,7 +33,6 @@ function ReviewsPage({ manga, reset, currUse, mangas, setVo, add }) {
       <button onClick={useReset}>X</button>
       <p>{obj.title}</p>
       <AddReview id={obj.id} currUse={currUse } add={setPageReviews} manga={manga} mangas={mangas}/>
-      {/* reviews are listed and each can be liked or disliked */}
       {revis.map(rv => <Reviews key={rv.id} rev={rv} />)}
       
     </div>
