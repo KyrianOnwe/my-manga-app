18b2cdff3be1cf31cb2f6333d057e14d3117a41c
Kyrian <dr.kyrian.onwe@gmail.com>
2022-12-23 14:21:14 -0600

    Cleanup Home

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Home() {
   return (
-    <div id='home-screen'><h3>"Welcome, and enjoy your time scoping out the manga we have here for you and please feel free to add more."</h3></div>
+    <div id='home-screen'><h2>"Welcome, and enjoy your time scoping out the manga we have here for you and please feel free to add more."</h2></div>
   )
 }
 
