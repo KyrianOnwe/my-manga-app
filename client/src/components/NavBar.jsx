850f46d80452fd6c4c57c3fdb4d8d382cbd60d32
Kyrian <dr.kyrian.onwe@gmail.com>
2022-12-23 14:16:54 -0600

    Cleanup Navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Logout from './Logout'
 
-function NavBar({ deleteUser, currUser, logged }) {
+function NavBar({ deleteUser, logged }) {
   
 
 
@@ -12,7 +12,6 @@ function NavBar({ deleteUser, currUser, logged }) {
     <div id='navbar'>
     <ul>
         <li><Link to="/">Home</Link></li>          
-        {/* <li><Link to="/reviews">Reviews</Link></li> */}
         <li><Link to="/mangas">Mangas</Link></li>
         <li><Link to="/mangas/new">Add a Fresh Mangas</Link></li>
        {logged ? <Logout deleteUser={deleteUser} /> : null }
