885bbafe6aa340d1a445d401aa68f90644db49cb
Kyrian <dr.kyrian.onwe@gmail.com>
2022-12-23 14:10:08 -0600

    Cleanup Reviews

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
-// import { Typography } from '@material-ui/core'
 
 
 function Reviews({ rev }) {
-  // when you click on a review, takes you to the ReviewsPage where you can like or dislike
   console.log(rev)
   return (
     <div>
