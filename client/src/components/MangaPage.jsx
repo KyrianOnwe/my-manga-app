4e0c4ef236e39fe94a5b4b6ce04f327121f64dbc
Kyrian <dr.kyrian.onwe@gmail.com>
2022-12-23 14:19:13 -0600

    Cleanup MangaPage

--- client/src/components/MangaPage.jsx
+++ client/src/components/MangaPage.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-// import Search from './Search'
 import MangasContainer from './MangasContainer'
 import ReviewsPage from './ReviewsPage'
 import { Container } from "@material-ui/core"
@@ -7,45 +6,21 @@ import { Container } from "@material-ui/core"
 function MangaPage({ mangas, currUse, logged, add }) {
   const [mangaChoice, setMangaChoice] = useState({})
   const [viewOne, setViewOne] = useState(false)
-  // const [change, setchange] = useState([])
-  // const [reviewPageInfo, setReviewPageInfo] = useState({})
   let mangasHere = mangaChoice  
   console.log( mangaChoice)
   function useSetMangaChoice(num){
     let x = mangas.find(m => m.id === num)
-    // console.log(num.id)
     setMangaChoice(x)
     setViewOne(!viewOne)
   }
   console.log(mangas)
-
-  // function useHandleMake(data){
-
-  // }
-
-
-  // useEffect(() => {
-  //   if(mangaChoice.x[0].id){
-  //     // console.log(mangaChoice)
-  //      setchange([...change, mangaChoice])
-  //   }
-
-  
-  // }, [mangaChoice, change])
 
   function resetMangaPage(){
     setMangaChoice({})
     setViewOne(false)
   }
 
-  function search(input){
-    mangas.filter(m => m.title === input)
-  }
 
-  function mangaAdd(data){
-    add(data)
-
-  }
 
   return (
     <>
@@ -53,7 +28,6 @@ function MangaPage({ mangas, currUse, logged, add }) {
       {viewOne ? <div>
         <ReviewsPage manga={mangasHere} reset={resetMangaPage} currUse={currUse} add={add} mangas={mangas} setVo={setViewOne}/>
       </div> : <div>
-        {/* <Search search={search}/> */}
         <MangasContainer mangas={mangas} choice={useSetMangaChoice} currUse={currUse} logged={logged} />
       </div> }
     </Container>
